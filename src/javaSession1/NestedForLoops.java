d7c7d0473ff452d8c473e6de5c050093c38fc1f4
ramvk576 <ramvk576@gmail.com>
2025-04-16 01:57:24 +0530

    training classes practical codes

---
+++ src/javaSession1/NestedForLoops.java
@@ -0,0 +1,20 @@
+package javaSession1;
+
+public class NestedForLoops {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=2;i++)
+		{
+		 for(int j=0;j<=5;j++)
+		 {
+		  System.out.print(i +"" +j);
+		  System.out.print(" ");		
+		 }
+		 System.out.println();
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
